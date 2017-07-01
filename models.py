e64aada7afc60cd35b632466191671b5fc87bed4
tmich <tiziano_michelessi@yahoo.it>
2017-07-02 01:53:25 +0200

    aggiunto campo 'stampato' alla tabella 'fattura'

--- models.py
+++ models.py
@@ -71,7 +71,7 @@ class Fattura(db.Model):
 			    backref = db.backref('fatture', lazy='dynamic'))
   voci = db.relationship('VoceFattura', cascade="save-update, merge, delete")
   canc = db.Column(db.Integer)
-  stampato = db.Column(db.Integer)
+  stampato = db.Column(db.Integer)	## ALTER TABLE `fattura` ADD COLUMN `stampato` INT NOT NULL DEFAULT '0' AFTER `canc`;
   
   def __init__(self, cliente, data, num = 0):
     self.cliente = cliente
@@ -180,72 +180,6 @@ class User(db.Model):
 
   def __repr__(self):
       return '<User %r>' % self.username
-
-# class FatturaTemp(db.Model):
-  # id = db.Column(db.Integer, primary_key=True)
-  # num = db.Column(db.Integer)
-  # data = db.Column(db.Date)
-  # n_scontr1 = db.Column(db.Integer)
-  # n_scontr2 = db.Column(db.Integer)
-  # n_scontr3 = db.Column(db.Integer)
-  # cliente_id = db.Column(db.Integer, db.ForeignKey('cliente.id'))
-  # cliente = db.relationship('Cliente',
-			    # backref = db.backref('fatturetemp', lazy='dynamic'))
-  # voci = db.relationship('VoceFatturaTemp', cascade="save-update, merge, delete")
-  # canc = db.Column(db.Integer)
-  
-  # def __init__(self, cliente, data, num = 0):
-    # self.cliente = cliente
-    # self.data = data
-    # self.num = num
-    # self.canc = 0
-  
-  # def imponibile(self):
-    # return sum([v.imponibile() for v in self.voci])
-  
-  # def iva(self):
-    # return sum([v.iva() for v in self.voci])
-
-  # def totale(self):
-    # return sum([v.totale() for v in self.voci])
-  
-  # def crea_voce(self, codart, descr, qta, prezzo, aliq):
-    # return VoceFatturaTemp(codart, descr, qta, prezzo, aliq)
-
-# class VoceFatturaTemp(db.Model):
-  # id = db.Column(db.Integer, primary_key=True)
-  # seq = db.Column(db.Integer)
-  # codart = db.Column(db.String(80))
-  # descr = db.Column(db.String(120))
-  # qta = db.Column(db.Integer)
-  # prezzo = db.Column(db.Numeric(10, 2)) 
-  # aliq = db.Column(db.Integer)
-  # fattura_id = db.Column(db.Integer, db.ForeignKey('fattura_temp.id'))
-  # canc = db.Column(db.Integer)
-  
-  # def __init__(self, codart, descr, qta, prezzo, aliq):
-    # self.codart=codart
-    # self.descr=descr
-    # self.qta=qta
-    # self.prezzo=prezzo
-    # self.aliq=aliq
-    # self.canc = 0
-  
-  # def iva(self):
-    # p = self.prezzo * self.qta
-    # iva = Decimal(p) - Decimal(self.imponibile())
-    # return round(iva, 2)
-  
-  # def imponibile(self):
-    # p = self.prezzo * self.qta
-    # al = Decimal(round(self.aliq, 2))
-    # cnv = Decimal(al/100)+1
-    # imponibile = Decimal(p / cnv, 2)
-    # return round(imponibile, 2)
-  
-  # def totale(self):
-    # tot=self.iva()+self.imponibile()
-    # return tot
     
 class FatturaSequence():
   def next_val(self, year):
@@ -314,75 +248,3 @@ class ObjFatt:
   def totale(self):
     return sum([v.totale() for v in self.voci])
 	
-# class TVoceFattura:
-  # codart = ''
-  # descr = ''
-  # qta = 1
-  # prezzo = 0.00 
-  # aliq = 0
-  # n_scontr1 = 0
-  # n_scontr2 = 0
-  # n_scontr3 = 0
-  
-  # def __init__(self, codart, descr, qta, prezzo, aliq):
-    # self.codart = codart
-    # self.descr = descr
-    # self.qta = qta
-    # self.prezzo=prezzo
-    # self.aliq=aliq
-    
-  # def serialize(self):
-    # return {
-      # 'codart': self.codart, 
-      # 'descr': self.descr,
-      # 'qta': self.qta,
-      # 'prezzo': self.prezzo,
-      # 'aliq': self.aliq
-    # }
-    
-  # def iva(self):
-    # p = self.prezzo * self.qta
-    # iva = Decimal(p) - Decimal(self.imponibile())
-    # return round(iva, 2)
-  
-  # def imponibile(self):
-    # p = self.prezzo * self.qta
-    # al = Decimal(round(self.aliq, 2))
-    # cnv = Decimal(al/100)+1
-    # imponibile = Decimal(p / cnv, 2)
-    # return round(imponibile, 2)
-  
-  # def totale(self):
-    # tot=self.iva()+self.imponibile()
-    # return tot
-  
-# class TFattura:
-  # num = 0
-  # id_cliente = None
-  # data = None
-  # voci = None
-  
-  # def __init__(self, dt, id_cliente):
-    # self.data = dt
-    # self.id_cliente = id_cliente
-    # self.voci = []
-    
-  # def serialize(self):
-    # return {
-      # 'num': self.num, 
-      # 'data': self.data,
-      # 'voci': jsonify([v.serialize() for v in self.voci])
-    # }
-    
-  # def aggiungi(self, codart, descr, qta, prezzo, aliq):
-    # v = TVoceFattura(codart, descr, qta, prezzo, aliq)
-    # self.voci.append(v)
-    
-  # def imponibile(self):
-    # return sum([v.imponibile() for v in self.voci])
-  
-  # def iva(self):
-    # return sum([v.iva() for v in self.voci])
-
-  # def totale(self):
-    # return sum([v.totale() for v in self.voci])
